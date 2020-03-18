d79e279e0efffd3ad2686acc09c1de8ac28b72bf
Martin Dobbermann <martin.dobbermann@altow.de>
2020-03-18 14:53:52 +0100

    Implemented process naming

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -6,7 +6,11 @@ defmodule IslandsEngine.Game do
   @players [:player1, :player2]
 
   def start_link(name) when is_binary(name) do
-    GenServer.start_link(__MODULE__, name, [])
+    GenServer.start_link(__MODULE__, via_tuple(name), [])
+  end
+
+  def via_tuple(name) do
+    {:via, Registry, {Registry.Game, name}}
   end
 
   def add_player(game, name) when is_binary(name) do
